2f1e763c5c9827cad75ee97ce52a2ca2e96b3a26
Nguyen Thanh <149889814+SpringThanh@users.noreply.github.com>
2025-03-06 08:44:05 +0700

    Update Question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,27 +1,24 @@
-import React, { Component } from "react";
+// ./components/Question.js
+import React from "react";
 import Options from "./Option";
 
-class Question extends Component {
-  render() {
-    const { question, selectedOption, onOptionChange, onSubmit } = this.props;
-
-    return (
-      <div className="">
-        <h3>Question {question.id}</h3>
-        <h5 className="mt-2">{question.question}</h5>
-        <form onSubmit={onSubmit} className="mt-2 mb-2">
-          <Options
-            options={question.options}
-            selectedOption={selectedOption}
-            onOptionChange={onOptionChange}
-          />
-          <button type="submit" className="btn btn-primary mt-2">
-            SUBMIT
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+const Question = ({ question, selectedOption, onOptionChange, onSubmit }) => {
+  return (
+    <div>
+      <h3>Question {question.id}</h3>
+      <h5 className="mt-2">{question.question}</h5>
+      <form onSubmit={onSubmit} className="mt-2 mb-2">
+        <Options
+          options={question.options}
+          selectedOption={selectedOption}
+          onOptionChange={onOptionChange}
+        />
+        <button type="submit" className="btn btn-primary mt-2">
+          SUBMIT
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Question;
