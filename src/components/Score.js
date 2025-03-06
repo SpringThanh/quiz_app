c6ba2d7150cc8ffaf3deb4f9878035503606add7
Nguyen Thanh <149889814+SpringThanh@users.noreply.github.com>
2025-03-06 08:44:41 +0700

    Update Score.js

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+// ./components/Score.js
+import React from "react";
 import "../App.css";
-class Score extends Component {
-  render() {
-    const { score, onNextQuestion } = this.props;
-    return (
-      <div>
-        <h2>Results</h2>
-        <h4>Your score: {score}</h4>
-      </div>
-    );
-  }
-}
+
+const Score = ({ score }) => {
+  return (
+    <div>
+      <h2>Results</h2>
+      <h4>Your score: {score}</h4>
+    </div>
+  );
+};
+
 export default Score;
